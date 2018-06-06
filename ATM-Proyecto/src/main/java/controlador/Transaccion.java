96d54ddb5a5f0d7897c6e6622e9c213d0717ef1a
DANILO <DBELENO86@UAN.EDU.CO>
2018-06-06 01:24:18 -0500

    Solo falta subir retiro y deposito a la bd.

--- ATM-Proyecto/src/main/java/controlador/Transaccion.java
+++ ATM-Proyecto/src/main/java/controlador/Transaccion.java
@@ -1,60 +1,42 @@
 package controlador;
 // Transaccion.java
+
 // La superclase abstracta Transaccion representa una transacción con el ATM
 
-import modelo.BaseDatosBanco;
+import modelo.*;
 import vista.Pantalla;
 
-public abstract class Transaccion
-{
-   private int numeroCuenta; // indica la cuenta implicada
-   private Pantalla pantalla; // pantalla del ATM
-   private BaseDatosBanco baseDatosBanco; // base de datos de información de cuentas
+public abstract class Transaccion {
+	
+	private Cuenta cuentaU;
+	private Pantalla pantalla; // pantalla del ATM
 
-   // el constructor de Transaccion es invocado por las subclases mediante super()
-   public Transaccion( int numeroCuentaUsuario, Pantalla pantallaATM, 
-      BaseDatosBanco baseDatosBancoATM )
-   {
-      numeroCuenta = numeroCuentaUsuario;
-      pantalla = pantallaATM;
-      baseDatosBanco = baseDatosBancoATM;
-   } // fin del constructor de Transaccion
+	// el constructor de Transaccion es invocado por las subclases mediante super()
+	public Transaccion(Cuenta cuentaUsuario, Pantalla pantallaATM) {
+		cuentaU = cuentaUsuario;
+		pantalla = pantallaATM;
+	} // fin del constructor de Transaccion
 
-   // devuelve el número de cuenta 
-   public int obtenerNumeroCuenta()
-   {
-      return numeroCuenta; 
-   } // fin del método obtenerNumeroCuenta
+	// devuelve el número de cuenta
+	public int obtenerNumeroCuenta() {
+		return cuentaU.obtenerNumeroCuenta();
+	} // fin del método obtenerNumeroCuenta
+	
 
-   // devuelve una referencia a la pantalla
-   public Pantalla obtenerPantalla()
-   {
-      return pantalla;
-   } // fin del método obtenerPantalla
+	public Cuenta getCuentaU() {
+		return cuentaU;
+	}
 
-   // devuelve una referencia a la base de datos del banco
-   public BaseDatosBanco obtenerBaseDatosBanco()
-   {
-      return baseDatosBanco;
-   } // fin del método obtenerBaseDatosBanco
+	public void setCuentaU(Cuenta cuentaU) {
+		this.cuentaU = cuentaU;
+	}
 
-   // realiza la transacción (cada subclase sobrescribe este método)
-   abstract public void ejecutar();
+	// devuelve una referencia a la pantalla
+	public Pantalla obtenerPantalla() {
+		return pantalla;
+	} // fin del método obtenerPantalla
+
+
+	// realiza la transacción (cada subclase sobrescribe este método)
+	abstract public void ejecutar();
 } // fin de la clase Transaccion
-
-
-
-/**************************************************************************
- * (C) Copyright 1992-2012 por Deitel & Associates, Inc. y                *
- * Pearson Education, Inc. Todos los derechos reservados.                 *
- *                                                                        *
- * RENUNCIA: Los autores y el editor de este libro han realizado su mejor *
- * esfuerzo para preparar este libro. Esto incluye el desarrollo, la      *
- * investigación y prueba de las teorías y programas para determinar su   *
- * efectividad. Los autores y el editor no hacen ninguna garantía de      *
- * ningún tipo, expresa o implícita, en relación con estos programas o    *
- * con la documentación contenida en estos libros. Los autores y el       *
- * editor no serán responsables en ningún caso por los daños consecuentes *
- * en conexión con, o que surjan de, el suministro, desempeño o uso de    *
- * estos programas.                                                       *
- *************************************************************************/
